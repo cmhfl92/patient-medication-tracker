8af1d8002093ab356ae0e5cb2d3a88a5c7bd5731
Christa <christa.hegedus1@gmail.com>
2025-05-25 17:50:58 -0400

    added types for medications and prescriptions

---
+++ client/src/app/types/medication.ts
@@ -0,0 +1,7 @@
+export interface Medication {
+  id: number;
+  name: string;
+  dosage: string;
+  frequency: string;
+  cost: number; //double check?
+}
---
+++ client/src/app/types/prescriptions.ts
@@ -0,0 +1,8 @@
+export interface Perscriptions {
+  id: number;
+  medication_id: number;
+  patient_id: number;
+  status: string;
+  start_date: string;
+  //foreignkey
+}
