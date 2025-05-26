fd4132dcf77d6f710d401802e277df0b9d4bd6cc
Christa <christa.hegedus1@gmail.com>
2025-05-25 21:38:28 -0400

    updated routers in routers

--- server/routes/patients.ts
+++ server/routes/patients.ts
@@ -1,3 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('../helpers/asynHandlerPatient');
+
+const {
+  listPatients,
+  getPatientById,
+  createPatient,
+} = require('../controllers/patients');
+
+router.get('/', asyncHandler(listPatients));
+router.get('/:id', asyncHandler(getPatientById));
+router.post('/', asyncHandler(createPatient));
+
+module.exports = router;
