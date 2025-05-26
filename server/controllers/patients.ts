3a0d22a9dac8303983e9dd96b777aced1de8bc49
Christa <christa.hegedus1@gmail.com>
2025-05-25 21:26:35 -0400

    added get and create server calls

---
+++ server/controllers/patients.ts
@@ -0,0 +1,45 @@
+import { stringify } from 'querystring';
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let patients: any[] = []; //need help with this part, I know we are using the sql DB so I don't think I need this?
+let nextId = 1;
+
+async function listPatients(req: any, res: any) {
+  await delay(500);
+  res.json(patients);
+}
+
+async function getPatientById(req: any, res: any) {
+  await delay(400);
+  const id = parseInt(req.params.id, 10);
+  const patient = patients.find(patient => patient.id === id);
+
+  if (!patient) {
+    const err = new Error('Patient Not found');
+    throw err;
+  }
+  res.json(patient);
+}
+
+async function createPatient(req: any, res: any) {
+  await delay(400);
+  const { name, dob, email } = req.body;
+  if (typeof name !== 'string') {
+    const err = new Error('Name is required!');
+    throw err;
+  }
+  if (typeof dob !== 'string') {
+    const err = new Error('DOB is required!');
+    throw err;
+  }
+  if (typeof email !== 'string') {
+    const err = new Error('Email is required!');
+    throw err;
+  }
+  const patient = { id: nextId++, name, dob, email };
+  patients.push(patient);
+  res.status(201).json(patient);
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import { initDB } from './db/db';
 
+const patientsRouter = require('./routes/patients');
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use('/patients', patientsRouter);
+
+app.use(errorHandler);
 
 app.get('/patients', async (__, res) => {
   const db = await initDB();
